9186f94009a2b4074ebc96f99193f1a838bb9ffe
himanshugupta0158 <himanshugupta0158@gmail.com>
2025-06-13 12:03:31 +0530

    TDD step 1 - handle two comma seperated numbers and non-numeric string

--- calculator.py
+++ calculator.py
@@ -1,6 +1,31 @@
+from typing import Union
+
+
 class StringCalculator:
+    """
+    A simple string calculator that can add numbers from a string input.
+    It supports empty strings, single numbers, and comma-separated numbers.
+    """
 
     def add(self, numbers: str):
         if not numbers:
             return 0
+
+        numbers = self.__split_numbers(numbers)
+        return self.__sum_numbers(numbers)
+
+    def __split_numbers(self, numbers: str):
+        if "," in numbers:
+            return numbers.split(",")
         return int(numbers) if numbers.isdigit() else 0
+
+    def __sum_numbers(self, numbers: Union[int, list]):
+        if isinstance(numbers, int):
+            return numbers
+        if isinstance(numbers, list):
+            total = 0
+            for number in numbers:
+                if number.isdigit():
+                    total += int(number)
+            return total
+        return 0
--- test_calculator.py
+++ test_calculator.py
@@ -21,3 +21,16 @@ def test_single_number_input():
     calc = StringCalculator()
     assert calc.add("5") == 5, "Expected 5 for single number input"
     assert calc.add("?") == 0, "Expected 0 for single special symbol input"
+
+
+def test_two_numbers_input():
+    """
+    Test the StringCalculator with two numbers input.
+    This should return the sum of the two numbers if both are valid digits or numbers,
+    or the first number if the second is a special symbol and vice versa
+    else return 0 incase both are non-numeric strings.
+    """
+    calc = StringCalculator()
+    assert calc.add("3,4") == 7, "Expected 7 for input '3,4'"
+    assert calc.add("10,20") == 30, "Expected 30 for input '10,20'"
+    assert calc.add("5,?") == 5, "Expected 5 for input '5,?'"
