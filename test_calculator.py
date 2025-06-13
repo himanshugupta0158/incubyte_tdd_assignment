b68adc1fe93679a0a81617cba384ca6b6c5d615c
himanshugupta0158 <himanshugupta0158@gmail.com>
2025-06-13 12:14:17 +0530

    TDD step 2 - handled multiple comma seperated string numbers and non-numbers

--- test_calculator.py
+++ test_calculator.py
@@ -34,3 +34,29 @@ def test_two_numbers_input():
     assert calc.add("3,4") == 7, "Expected 7 for input '3,4'"
     assert calc.add("10,20") == 30, "Expected 30 for input '10,20'"
     assert calc.add("5,?") == 5, "Expected 5 for input '5,?'"
+
+def test_multiple_numbers_input():
+    """
+    Test the StringCalculator with multiple numbers input.
+    This should return the sum of all valid numbers in the string.
+    """
+
+    calc = StringCalculator()
+    assert calc.add("1,2,3") == 6, "Expected 6 for input '1,2,3'"
+    assert calc.add("10,20,30") == 60, "Expected 60 for input '10,20,30'"
+    assert calc.add("5,?,7") == 12, "Expected 12 for input '5,?,7'"
+    assert calc.add("?,10,20") == 30, "Expected 30 for input '?,10,20'"
+    assert calc.add("?,?,?") == 0, "Expected 0 for input '?,?,?'"
+    nums = ",".join(["2" for i in range(10)])
+    assert calc.add(nums) == 20, f"Expected 20 for input '{nums}'"
+
+def test_invalid_input():
+    """
+    Test the StringCalculator with invalid input.
+    This should return 0
+    """
+    calc = StringCalculator()
+    assert calc.add("a,b,c") == 0, "Expected 0 for input 'a,b,c'"
+    assert calc.add("1,2,a") == 3, "Expected 3 for input '1,2,a'"
+    assert calc.add("?") == 0, "Expected 0 for single special symbol input"
+    assert calc.add("?,?") == 0, "Expected 0 for two special symbols input"
